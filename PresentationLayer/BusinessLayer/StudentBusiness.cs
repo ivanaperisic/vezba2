cd62f88a310d698804daa3b8e56b57c5f4ab3b81
DESKTOP-EBD3442\HP <34-2018@ftn.edu.rs>
2020-12-02 22:08:29 +0100

    aaaaaaaa

---
+++ PresentationLayer/BusinessLayer/StudentBusiness.cs
@@ -0,0 +1,31 @@
+﻿
+using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class StudentBusiness
+    {
+            public StudentRepository sr;
+            public StudentBusiness()
+            {
+                this.sr = new StudentRepository();
+            }
+            public List<Student> GetStudents()
+            {
+                return this.sr.GetStudents();
+            }
+            public bool insertStudent(Student s)
+            {
+                int result = this.sr.insertStudent(s);
+                if (result != 0)
+                    return true;
+                return false;
+
+            }
+        }
+    }
---
+++ PresentationLayer/DataLayer/StudentRepository.cs
@@ -0,0 +1,56 @@
+﻿
+using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class StudentRepository
+    {
+        public List<Student> GetStudents()
+        {
+            List<Student> studentList = new List<Student>();
+            string conString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=FacultyDB2;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            using (SqlConnection con = new SqlConnection(conString)) 
+            {
+                con.Open();
+                string CommandText = "Select * from Students";
+                SqlCommand com = new SqlCommand(CommandText, con);
+                SqlDataReader dr = com.ExecuteReader();
+                while (dr.Read())
+                {
+                    Student s = new Student(dr.GetInt32(0), dr.GetString(1), dr.GetString(2), dr.GetDecimal(3));
+                    studentList.Add(s);
+                }
+            }
+            return studentList;
+
+        }
+
+
+
+        public int insertStudent(Student s)
+        {
+            int result;
+            string conString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=FacultyDB2;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            using (SqlConnection con = new SqlConnection(conString)) 
+            {
+                con.Open();
+                string CommandText = "Insert into Students(name,indexNumber,averageMark) values(@name,@indexNumber,@averageMark)";
+                SqlCommand com = new SqlCommand(CommandText, con);
+                com.Parameters.AddWithValue("@name", s.name);
+                com.Parameters.AddWithValue("@indexNumber", s.indexNumber);
+                com.Parameters.AddWithValue("@averageMark", s.averageMark);
+                result = com.ExecuteNonQuery();
+
+            }
+
+
+            return result;
+        }
+    }
+}
